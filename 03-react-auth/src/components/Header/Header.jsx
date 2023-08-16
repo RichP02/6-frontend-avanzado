40ba20d9ba301c84fed7695a38705111b94a9476
Ricardo <ricardoparedesg16@gmail.com>
2023-08-15 21:08:07 -0600

    Configuracion de header.jsx

--- 03-react-auth/src/components/Header/Header.jsx
+++ 03-react-auth/src/components/Header/Header.jsx
@@ -2,7 +2,26 @@ import './header.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className='header'>
+      <a href='/' className='header__logo'>LOGO</a>
+      <ul className='header__navList'>
+        <li className='header__listItem'>
+          <a href='/' className='header__itemLink header__itemLink--isActive'>Home</a>
+        </li>
+        <li className='header__listItem'>
+          <a href='/dashboard' className='header__itemLink'>Dashboard</a>
+        </li>
+        <li className='header__listItem'>
+          <a href='/secret' className='header__itemLink'>Secret</a>
+        </li>
+        <li className='header__listItem'>
+          <a href='/login' className='header__itemLink'>Login</a>
+        </li>
+        <li className='header__listItem'>
+          <a href='/signup' className='header__itemLink'>Signup</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
