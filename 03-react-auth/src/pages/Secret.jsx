46fdec83b3cce0c02a2e52019c051cdf2f980478
Ricardo <ricardoparedesg16@gmail.com>
2023-08-22 20:09:33 -0600

    Modificacion en login, singup y secret

--- 03-react-auth/src/pages/Secret.jsx
+++ 03-react-auth/src/pages/Secret.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react"
+import { getAllUsersService } from "../services/userService"
+
 const Secret = () => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const fetchUsersData = async () => {
+      try {
+        const token = localStorage.getItem("jwt_token")
+        const response = await getAllUsersService(token)
+        setUsers(response)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchUsersData()
+  }, [])
+
   return (
-    <h1>Secret</h1>
+    <>
+      <h1>Secret</h1>
+      <div>
+        {users.map(({ id, first_name, last_name, gender, email}) => 
+        (<div key={id} >
+          <span>{first_name}</span><br />
+          <span>{last_name}</span><br />
+          <span>{gender}</span><br />
+          <span>{email}</span><br />
+        </div>)
+        ) }
+      </div>
+    </>
   )
 }
 export default Secret
